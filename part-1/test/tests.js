5ae33c867dd65fcfd1debc1c19dc473b2a35a442
ontoneio <ontoneio.mm@gmail.com>
2017-08-14 16:54:32 -0700

    Adds more tests to tests.js

--- part-1/test/tests.js
+++ part-1/test/tests.js
@@ -4,25 +4,45 @@ const expect = require('chai').expect
 describe("Phase 2-c part-1 functions", function() {
     
     context("weekday()", function() {
-        it("", function() {
+        it("Returns the day of the week as a string from a valid Date Object", function() {
+            
+        })
+    })
+    context("weekday()", function() {
+        it("Checks that function throws error when not given a proper Date Object type of argument", function() {
             
         })
     })
 
     context("capitalizeFourth()", function() {
-        it("", function() {
+        it("Takes a string and returns every 4th character converted to upper case and the rest converted to lower case", function() {
+            
+        })
+    })
+    context("capitalizeFourth()", function() {
+        it("Checks that function throws error when not given a proper string type of argument", function() {
             
         })
     })
 
     context("getValues()", function() {
-        it("", function() {
+        it("Returns all own values from given object argument", function() {
+            
+        })
+    })
+    context("getValues()", function() {
+        it("Checks that function throws error when not given a proper object type of argument", function() {
             
         })
     })
 
     context("filterAround()", function() {
-        it("", function() {
+        it("returns a new array containing only the elements from the source array that come before lower alphabetically and after upper", function() {
+            
+        })
+    })
+    context("filterAround()", function() {
+        it("Checks that function throws error when not given a proper types of arguments", function() {
             
         })
     })
