db72ce0434183418ba59c252baa232ecf7f0152b
ontoneio <ontoneio.mm@gmail.com>
2017-08-14 16:29:43 -0700

    Sets up structure for mocha chai test

---
+++ part-1/functions.js
@@ -0,0 +1,19 @@
+module.exports = {
+
+    weekday(date) {
+
+    },
+
+    capitalizeFourth(string) {
+
+    },
+
+    getValues(obj) {
+
+    },
+
+    filterAround(array, lower, upper) {
+
+    }
+
+}
---
+++ part-1/test/tests.js
@@ -0,0 +1,29 @@
+const p2 = require('../functions')
+const expect = require('chai').expect
+
+describe("Phase 2-c part-1 functions", function() {
+    
+    context("weekday()", function() {
+        it("", function() {
+            
+        })
+    })
+
+    context("capitalizeFourth()", function() {
+        it("", function() {
+            
+        })
+    })
+
+    context("getValues()", function() {
+        it("", function() {
+            
+        })
+    })
+
+    context("filterAround()", function() {
+        it("", function() {
+            
+        })
+    })
+})
