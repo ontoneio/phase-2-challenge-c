8d7b54d94b187fddd3d1a859a9ff7d13fcf0635f
ontoneio <ontoneio.mm@gmail.com>
2017-08-14 17:29:35 -0700

    Adds invalid and valid tests to capitalizeFourth()

--- part-1/test/tests.js
+++ part-1/test/tests.js
@@ -40,11 +40,34 @@ describe("Phase 2-c part-1 functions", function() {
     context("capitalizeFourth()", function() {
         it("Takes a string and returns every 4th character converted to upper case and the rest converted to lower case", function() {
             
+            const validString1 = 'This is a valid string to parse'
+            const validString2 = 'Another String OF ALL KINDS of letters'
+            const validString3 = 'Open SAYS A ME'
+
+            const validTest1 = p2.capitalizeFourth(validString1)
+            const validTest2 = p2.capitalizeFourth(validString2)
+            const validTest3 = p2.capitalizeFourth(validString3)
+
+            expect(validTest1).to.be.equal('thiS is a vAlid strIng To pArse')
+            expect(validTest2).to.be.equal('anoTher strIng Of aLl kInds of LettErs')
+            expect(validTest3).to.be.equal('opeN saYs a me')
+
         })
     })
     context("capitalizeFourth()", function() {
         it("Checks that function throws error when not given a proper string type of argument", function() {
+
+            const invalidType1 = 2005
+            const invalidType2 = ['An', 'Invalid', 'Array']
+            const invalidType3 = {'some': 1, 'object': 2, 'literal': 3}
+
+            const invalidTest1 = () => p2.capitalizeFourth(invalidType1)
+            const invalidTest2 = () => p2.capitalizeFourth(invalidType2)
+            const invalidTest3 = () => p2.capitalizeFourth(invalidType3)
             
+            expect(invalidTest1).to.throw(Error)
+            expect(invalidTest2).to.throw(Error)
+            expect(invalidTest3).to.throw(Error)
         })
     })
 
