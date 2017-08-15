5ea2f4beaf926683a99bea56520bcf5f3b0bd68f
ontoneio <ontoneio.mm@gmail.com>
2017-08-15 00:04:26 -0700

    Valid and Invalid tests passing for weekday()

--- part-1/functions.js
+++ part-1/functions.js
@@ -1,7 +1,16 @@
 module.exports = {
 
     weekday(date) {
+        const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+        const callableDate = Object.prototype.toString.call(date)
 
+        if (!callableDate === '[object Date]') { 
+            throw new Error("Not a proper Date object for function argument")
+        }
+        else {
+            console.log(daysOfWeek[date.getDay()]);                    
+            return daysOfWeek[date.getDay()]            
+        }
     },
 
     capitalizeFourth(string) {
--- part-1/test/tests.js
+++ part-1/test/tests.js
@@ -6,12 +6,12 @@ describe("Phase 2-c part-1 functions", function() {
     context("weekday()", function() {
         it("Returns the day of the week as a string from a valid Date Object", function() {
 
-            const validDate1 = new Date(1987, 1, 1) // Should Return Thursday
-            const validDate2 = new Date(2017, 4, 24) // Should Return Monday
-            const validDate3 = new Date(1999, 7, 4) // Should Return Sunday
+            const validDate1 = new Date('January 01, 1987, 08:30:30') // Should Return Thursday
+            const validDate2 = new Date(2017, 3, 24) // Should Return Monday
+            const validDate3 = new Date(1999, 6, 04) // Should Return Sunday
 
             const validTest1 = p2.weekday(validDate1)
-            const validTest2 = p2.weekday(validDate2)
+            const validTest2 = p2.weekday(validDate2)   
             const validTest3 = p2.weekday(validDate3)
 
             expect(validTest1).to.be.equal('Thursday')
@@ -23,13 +23,13 @@ describe("Phase 2-c part-1 functions", function() {
     context("weekday()", function() {
         it("Checks that function throws error when not given a proper Date Object type of argument", function() {
             
-            const invalidDate1 = 'January 1st, 1987'
-            const invalidDate2 = 20170424
+            const invalidDate1 = 'Four score and seven years ago'
+            const invalidDate2 = 20170424097890709790789780
             const invalidDate3 = new Date('Not a real date')
 
-            const invalidTest1 = () => p2.weekday(invalidDate1)
-            const invalidTest2 = () => p2.weekday(invalidDate2)
-            const invalidTest3 = () => p2.weekday(invalidDate3)
+            const invalidTest1 = p2.weekday.bind(invalidDate1)
+            const invalidTest2 = p2.weekday.bind(invalidDate2)
+            const invalidTest3 = p2.weekday.bind(invalidDate3)
 
             expect(invalidTest1).to.throw(Error)
             expect(invalidTest2).to.throw(Error)
