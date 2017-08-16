356433c9e7edcbf7af2bb72bc563fb713e31d876
ontoneio <ontoneio.mm@gmail.com>
2017-08-16 08:16:06 -0700

    Both search functions working

--- part-2/searchByModel.js
+++ part-2/searchByModel.js
@@ -2,13 +2,12 @@ const fs = require('fs')
 
 let carModel = process.argv[2]
 let carsFile = fs.readFileSync('./cars.json', 'utf8')
-let resultsFound = []
+
 
 if (!carModel) { console.error('\nNo arguments passed.\nPlease specify a model of car :-)\n')}
 
 else{   console.log(`\nFinding cars with model "${carModel}"...\n`); 
         getCarByModel(carModel, carsFile) }
-
 
 
 function formatJSONreviver(propertyName, propertyValue) {
@@ -24,8 +23,6 @@ function getCarByModel(carModel, carFile) {
     let parsedFile = JSON.parse(carsFile, formatJSONreviver)
     let carModelCase = carModel.toLowerCase()
 
-    // let foundModel = {}
-
     let foundModels = parsedFile.filter((carModel, index) => {
         if(carModel.model.toLowerCase() === carModelCase) {
             return carModel
@@ -39,3 +36,4 @@ function getCarByModel(carModel, carFile) {
     else {
         console.log(foundModels);        
     }
+}
---
+++ part-2/searchInYears.js
@@ -0,0 +1,68 @@
+const fs = require('fs')
+
+let yearRange = process.argv[2]
+let carsFile = fs.readFileSync('./cars.json', 'utf8')
+let yearsAsNumber = []
+
+
+if (!checkYearRangeisValid()) 
+    { 
+        console.error('\nNo valid arguments passed.\nPlease specify a year range as YYYY-YYYY format\n')
+    }
+
+else{   
+        console.log(`\nFinding cars made between ${yearRange}...\n`);
+        getCarByYear(yearRange, carsFile) 
+    }
+
+
+    
+function checkYearRangeisValid() {
+        
+    let yearRangeFormat = /^(\d{4})-(\d{4})/g.test(yearRange)
+
+    if (!yearRangeFormat) {
+        return false
+    }
+    else {        
+        return true
+    }   
+}
+
+function splitYearRange(yearRange) {
+    let yearRangeSplit = yearRange.split('-')
+    let yearLower = parseInt(yearRangeSplit[0], 10)
+    let yearUpper = parseInt(yearRangeSplit[1], 10)
+
+    return yearsAsNumber = [yearLower, yearUpper]
+}
+
+function formatJSONreviver(propertyName, propertyValue) {
+    
+    if (propertyName === "vin" || propertyName === "last_owner" || propertyName === "date_purchased") {
+        return undefined;
+    }
+    return propertyValue 
+}
+
+
+function getCarByYear(yearRange, carFile) { 
+    
+    let parsedFile = JSON.parse(carsFile, formatJSONreviver, 1)
+    let yearsAsNumber = splitYearRange(yearRange)
+
+
+    let foundModels = parsedFile.filter((carModel) => {
+        if (carModel.year <= yearsAsNumber[1] && carModel.year >= yearsAsNumber[0]) {
+            return carModel
+            }       
+        }
+    )
+
+    if (foundModels.length === 0) {
+        console.warn(`\nNo models found within "${yearRange}"...\n`);
+    }
+    else {
+        console.log(foundModels);        
+    }
+}
